75ce1f7dbcf018f480c54770341b5d3db7dd9f1e
Joe Wolfgram <joeywolfgram@gmail.com>
2015-12-16 21:46:34 -0100

    Building Landing page, stopped at Overview tabs. -Added logo.png to /images -Added Ein.js which will help with the Overview on the landing page. -Added background images and for the Carousel. -Changed buttons for New and Join game to <div>

--- public/js/ein.js
+++ public/js/ein.js
@@ -1,5 +1,45 @@
-var menuGame = getElementById('menu-game');
+var newBtn = document.getElementById('new-game'),
+joinBtn = document.getElementById('join-game'),
+overview = document.getElementById('info-overview'),
+howPlay = document.getElementById('info-howtoplay'),
+tips = document.getElementById('info-tips'),
+tabOverview = document.getElementById('tab-overview'),
+tabhowPlay = document.getElementById('tab-howtoplay'),
+tabTips = document.getElementById('tab-tips');
 
-menuGame.addEventListener('click', function(){
-  $('.collapse').collapse();
+newBtn.addEventListener('mouseover', function(){
+  newBtn.setAttribute('class', 'button-hover');
+  console.log('PASSED');
 }, false);
+
+joinBtn.addEventListener('click', function(){
+  joinBtn.setAttribute('class', 'button-hover');
+}, false);
+
+
+
+
+
+
+
+
+
+//Overview, How to Play, Tips selector
+overview.addEventListener('click', function(){
+  tabOverview.setAttribute('class', '');
+  tabhowPlay.setAttribute('class', 'hidden');
+  tabTips.setAttribute('class', 'hidden');
+}, false);
+
+howPlay.addEventListener('click', function(){
+  tabhowPlay.setAttribute('class', '');
+  tabOverview.setAttribute('class', 'hidden');
+  tabTips.setAttribute('class', 'hidden');
+}, false);
+
+tips.addEventListener('click', function(){
+  tabTips.setAttribute('class', '');
+  tabhowPlay.setAttribute('class', 'hidden');
+  tabOverview.setAttribute('class', 'hidden');
+}, false);
+
