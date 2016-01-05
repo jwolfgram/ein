f85d240ec9a4b46c2502ef0d2c759642d684c038
Joe Wolfgram <joeywolfgram@gmail.com>
2016-01-05 10:14:24 -0800

    Socket.io integration-- -node will now send table card update to all clients and update card on public table.

--- http.js
+++ http.js
@@ -61,9 +61,12 @@ io.on('connection', function (socket) {
   if (playCard.game[0].session === 'Waiting for Players') {
     for (i = 0; i < playCard.players.length; i++) {
       if (i === 3) { //Checking if all player slots are filled, need this before break
+        var turn = playCard.game[0].turn;
         playCard.game[0].session = 'Game in Session'; //Every update game.js will redraw table
-        socket.emit('status', 'Game in Session');
-        console.log('Assigned Game in Session status');
+        io.emit('status', 'Game in Session');  //Client screen will go 'yay! game started'
+        io.emit('table', playCard.table[0]); //Send table card data to client to populate current card.
+        io.emit('status', playCard.players[turn].id); //Client will check to see if the id matches themselves and annonce if its their turn or not
+        console.log('The game has started and its ' + playCard.players[turn].id + ' turn.');
       }
       if (playCard.players[i].id === socket.id) { //Check if existing player connected for status update
         socket.emit('status', playCard.game[0].session);
@@ -71,26 +74,40 @@ io.on('connection', function (socket) {
       else {
         if (playCard.players[i].id === 99) {
           playCard.players[i].id = socket.id;
-          console.log('Player ' + socket.id + ' has joined and been loggged... ' +  playCard.players[i].id);
+          console.log('Player ' + socket.id + ' has joined the current game session. ' +  playCard.players[i].id);
           break;
         }
       }
     }
   }
-  else { //Lets check to make sure game is really in session....
-    if (playCard.game[0].session === 'Game in Session') {
-      var turn = playCard.game[0].turn;
-      var sessionID = playCard.players[turn].id;
-      socket.emit('table', playCard.table[0]);
-      if (socket.id === sessionID) {
-        socket.emit('cards', playCard.players[turn].cards);
-        playCard.game[0].turn++;
-      }
-      else {
-        socket.emit('status', 'Player: ' + turn + ' turn.'); // If this happens hopefully client will respond and not allow plays
+  else { //Game is full, see if game is over and reset game for new one if needed.
+    for (i = 0; i < playCard.players.length; i++) {
+      if (playCard.players[i].length === 0) {
+        playCard.game[0].session = 'Game End';
       }
     }
+    if (playCard.game[0].session === 'Game End') {
+      for (i = 0; i < playCard.players.length; i++) {
+        playCard.players[i].id = 99;
+      }
+    }
+    socket.emit('status', 'Game is full'); //Send private message to the fifth client joining saying the game is full
   }
+});
+
+io.on('play', function (socket) {
+  var turn = playCard.game[0].turn;
+  var playerTurn = playCard.players[turn].id;
+  if (socket.id === playerTurn) {
+    //Basically, check if the incomming card is valid in players deck and remove card from their deck
+    socket.emit('cards', playCard.players[turn].cards);
+    //determine if card was valid or if player needs to play correct card before incrementing turn, also include if statement if it needs to reset counter to 0
+    playCard.game[0].turn++;
+  }
+  else {
+    io.emit('status', playerTurn); // If this then resend data to clients saying whose turn it is to active eventlistener to stop incorrect players playing
+  }
+  io.emit('table', playCard.table[0]); //Since play was made, resend the current card on table to players to see current card
 });
 
 console.log(port + ' is the port your webserver is running on.');
--- public/js/game.js
+++ public/js/game.js
@@ -9,242 +9,30 @@ game,
 player,
 socket = io.connect('http://localhost:8080');
 
-window.onload = function() {
-  socket.on('playersDeck', function (data) {
-    console.log("Player Deck:", data);
-  });
-  socket.on('table', function (data) {
-    console.log("Table:", data);
-  });
-  socket.on('status', function (data) {
-    console.log("Status:", data);
-  });
-};
+socket.on('connect', function () {
+    console.log('Connected!');
+});
 
-function getPlayer() {
-  xhr.open('GET', '/game-state/', false);
-  console.log('xhr status: ' + xhr.status);
-  xhr.send();
-  console.log('xhr status: (before player defined) ' + xhr.status);
-  player = xhr.response;
-  console.log('xhr status: ' + xhr.status);
-  console.log('Player number from server: ' + xhr.response);
-  //getGame();
-}
+socket.on('cards', function (data) { //When server sends players deck we will need to redraw the players deck.
+  console.log('Player Deck:' + data);
+});
 
-//Promise.all([getPlayer()]);
-//getGame();
-
-function getGame() {
-  var cardsTwo,
-  cardsThree,
-  cardsFour,
-  twoDiv = document.createElement('p'),
-  twoText,
-  threeDiv = document.createElement('p'),
-  threeText,
-  fourDiv,
-  fourText,
-  path = '/game-state/' + player;
-
-  console.log('This is path: ' + path);
-  xhr.open('GET', path, true);
-  console.log('xhr status: ' + xhr.status);
-  xhr.send();
-  console.log('xhr status: ' + xhr.status);
-  xhr.onload = function() {
-    console.log('xhr status: ' + xhr.status);
-    game = JSON.parse(xhr.response);
-    console.log('xhr status: ' + xhr.status);
-    drawGame();
-
-  };
-}
-
-function drawGame() {
-  //Wipe 'table' of all Player stats and cards.
-  while (playerHand.hasChildNodes()) { //Check to see if we need to clean the table and reset....
-    playerHand.removeChild(playerHand.lastChild);
-  }
-  if (twoCard.hasChildNodes()) {
-    twoCard.removeChild(twoCard.lastChild);
-  }
-  if (threeCard.hasChildNodes()) {
-    threeCard.removeChild(threeCard.lastChild);
-  }
-  if (fourCard.hasChildNodes()) {
-    fourCard.removeChild(fourCard.lastChild);
-  }
-  if (tableCard.hasChildNodes()) {
+socket.on('table', function (data) { //When server sends current card on table, we will need to update the card on the table.
+  console.log('Table:' + data);
+  console.log('Color: ' + data.color);
+  console.log('Number: ' + data.number);
+  //console.log('Number: ' + card.["number"]);
+  while (tableCard.hasChildNodes()) { //Go ahead and remove the number on the table card so we can repopulate it.
     tableCard.removeChild(tableCard.lastChild);
   }
-  //Get players hand of cards.
-  cardCount = game.players[player].cards.length;
-  console.log('Cards counted: ' + cardCount);
-  for (var i = 0; i < cardCount; i++) {
-    var makeCard = document.createElement('div'); //Make card div for webpage
-    var cardColor = game.players[player].cards[i].color;
-    makeCard.setAttribute("class", cardColor + "-card"); //add class to this for the colors.
-    var cardNum = document.createElement('p'); //Creates p tag for div with card number
-    var pText = document.createTextNode(game.players[player].cards[i].number); //This is the number of the Ein card
-    cardNum.appendChild(pText); //getting text for p tag <p>1</p>
-    makeCard.appendChild(cardNum); //appending p tag to card div <div><p>1</p></div>
-    playerHand.appendChild(makeCard); //Placing card in the players hand
-  }
-
-  twoDiv = document.createElement('p');
-  twoCard.appendChild(twoDiv);
-
-  threeDiv = document.createElement('p');
-  threeCard.appendChild(threeDiv);
-
-  fourDiv = document.createElement('p');
-  fourCard.appendChild(fourDiv);
-  //Write number of remaining cards on ememies cards.
-  switch (player) {
-    case "0": //Select PLayer 1
-    cardsTwo = game.players[1].cards.length;
-    cardsThree = game.players[2].cards.length;
-    cardsFour = game.players[3].cards.length;
-    console.log('Player hand: ' + game.players[0].cards.length);
-
-    twoText = document.createTextNode('Cards: ' + game.players[1].cards.length);
-    twoDiv.appendChild(twoText);
-
-    threeText = document.createTextNode('Cards: ' + game.players[2].cards.length);
-    threeDiv.appendChild(threeText);
-
-    fourText = document.createTextNode('Cards: ' + game.players[3].cards.length);
-    fourDiv.appendChild(fourText);
-    break;
-
-    case 1: //Select PLayer 2
-    cardsTwo = game.players[0].cards.length;
-    cardsThree = game.players[2].cards.length;
-    cardsFour = game.players[3].cards.length;
-    console.log('Player hand: ' + game.players[1].cards.length);
-
-    twoText = document.createTextNode('Cards: ' + game.players[1].cards.length);
-    twoDiv.appendChild(twoText);
-
-    threeText = document.createTextNode('Cards: ' + game.players[2].cards.length);
-    threeDiv.appendChild(threeText);
-
-    fourText = document.createTextNode('Cards: ' + game.players[3].cards.length);
-    fourDiv.appendChild(fourText);
-    break;
-
-    case 2: //Select PLayer 3
-    cardsTwo = game.players[0].cards.length;
-    cardsThree = game.players[1].cards.length;
-    cardsFour = game.players[3].cards.length;
-    console.log('Player hand: ' + game.players[2].cards.length);
-
-    twoText = document.createTextNode('Cards: ' + game.players[1].cards.length);
-    twoDiv.appendChild(twoText);
-
-    threeText = document.createTextNode('Cards: ' + game.players[2].cards.length);
-    threeDiv.appendChild(threeText);
-
-    fourText = document.createTextNode('Cards: ' + game.players[3].cards.length);
-    fourDiv.appendChild(fourText);
-    break;
-
-    case 3:  //Select PLayer 4
-    cardsTwo = game.players[0].cards.length;
-    cardsThree = game.players[1].cards.length;
-    cardsFour = game.players[2].cards.length;
-    console.log('Player hand: ' + game.players[3].cards.length);
-
-    twoText = document.createTextNode('Cards: ' + game.players[1].cards.length);
-    twoDiv.appendChild(twoText);
-
-    threeText = document.createTextNode('Cards: ' + game.players[2].cards.length);
-    threeDiv.appendChild(threeText);
-
-    fourText = document.createTextNode('Cards: ' + game.players[3].cards.length);
-    fourDiv.appendChild(fourText);
-    break;
-
-    default:
-    console.log('There was an error with the switch statement in game.js the function enemyCard(): ' + player);
-    break;
-  }
-  //Draw playing card on table.
-  tableCard.setAttribute("class", game.table[0].color + "-card center-block");
+  tableCard.setAttribute("class", data.color + "-card center-block");
   var tableNum = document.createElement('p');
-  var tableText = document.createTextNode(game.table[0].number);
+  var tableText = document.createTextNode(data.number);
   tableNum.appendChild(tableText);
   tableCard.appendChild(tableNum);
-}
+});
 
-function sendTurn() {
-  console.log('Sending Card: ' + cardSelect);
-  var number = cardSelect.textContent;
-  var turnPlayed;
-  if (cardSelect.classList.contains('blue-card')) {
-    turnPlayed = [number, "blue", player];
-  }
-  if (cardSelect.classList.contains('yellow-card')) {
-    turnPlayed = [number, "yellow", player];
-  }
-  if (cardSelect.classList.contains('green-card')) {
-    turnPlayed = [number, "green", player];
-  }
-  if (cardSelect.classList.contains('red-card')) {
-    turnPlayed = [number, "red", player];
-  }
-  turnPlayed = JSON.stringify(turnPlayed);
-  console.log(turnPlayed + 'Right before shit goes down...');
-  console.log('xhr status: ' + xhr.status);
-  xhr.open('POST', '/game-turn', true);
-  console.log('xhr status: ' + xhr.status);
-  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  console.log('xhr status:content-type ' + xhr.status);
-  xhr.send(turnPlayed);
-  console.log('xhr status:last ' + xhr.status);
-  if (xhr.readyState == 4) {
-    console.log('xhr status (onload function): ' + xhr.status);
-    setTable(); //redraw table
-    console.log('xhr status final?: ' + xhr.status);
-  }
-}
-
-function selectCard(e) {
-  if (e.target !== e.currentTarget) {
-    if (e.target.nodeName === 'P') {
-      cardSelect = e.target.parentElement;
-    }
-    else {
-      cardSelect = e.target;
-    }
-  }
-  console.log('Found card to play and selected!' + cardSelect);
-  e.stopPropagation();
-}
-
-playerHand.addEventListener("click", selectCard, false); //Adding event listener for 'Playing Card'
-
-
-submitBtn.addEventListener('click', function(){
-  console.log('Got it.play-card' + cardSelect);
-  console.log(number);
-  var number = cardSelect.textContent;
-  console.log(number);
-  var turnPlayed;
-  if (cardSelect.classList.contains('blue-card')) {
-    turnPlayed = [number, "blue", player];
-  }
-  if (cardSelect.classList.contains('yellow-card')) {
-    turnPlayed = [number, "yellow", player];
-  }
-  if (cardSelect.classList.contains('green-card')) {
-    turnPlayed = [number, "green", player];
-  }
-  if (cardSelect.classList.contains('red-card')) {
-    turnPlayed = [number, "red", player];
-  }
-  turnPlayed = JSON.stringify(turnPlayed);
-
-  Promise.all([sendTurn(),getGame(),drawGame()]);
-}, false);
+socket.on('status', function (data) { //When we get a new status, such as the playerid (if playerID matches client then announce to player its their turn)
+  console.log('Status / Whose turn it is:' + data);
+  console.log('My id is: ' + socket.id);
+});
